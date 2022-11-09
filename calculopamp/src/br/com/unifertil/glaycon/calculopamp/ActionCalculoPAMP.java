9fb4874f5aace32b3ce9a1ac53145f5384eadbaf
glazau <glaycon.hbp@gmail.com>
2022-11-09 00:06:03 -0300

    first commit

---
+++ calculopamp/src/br/com/unifertil/glaycon/calculopamp/ActionCalculoPAMP.java
@@ -0,0 +1,99 @@
+package br.com.unifertil.glaycon.calculopamp;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.EntityFacade;
+import br.com.sankhya.jape.core.JapeSession;
+import br.com.sankhya.jape.dao.JdbcWrapper;
+import br.com.sankhya.jape.sql.NativeSql;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.MGEModelException;
+import br.com.sankhya.modelcore.util.EntityFacadeFactory;
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+
+public class ActionCalculoPAMP implements AcaoRotinaJava {
+
+    @Override
+    public void doAction(ContextoAcao contextoAcao) throws Exception {
+        JapeSession.SessionHandle hnd = null;
+        JdbcWrapper jdbcWrapper = null;
+        NativeSql nativeSql = null;
+        try {
+            hnd = JapeSession.open();
+            Registro[] ordens = contextoAcao.getLinhas();
+            if (ordens.length == 0) {
+                contextoAcao.mostraErro("Selecione pelo menos uma linha.");
+            } else {
+                for (Registro registroSelcionado : ordens) {
+                    BigDecimal seqAmostra = (BigDecimal) registroSelcionado.getCampo("SEQAMOSTRA");
+                    BigDecimal sequencia = (BigDecimal) registroSelcionado.getCampo("SEQUENCIA");
+                    String resultadoAnaliseQuimicaStringComVirgula = (String) registroSelcionado.getCampo("RESULTADO");
+                    String resultadoAnaliseQuimicaString = resultadoAnaliseQuimicaStringComVirgula.replace(',','.');
+                    BigDecimal tolerancia;
+                    DynamicVO controleAmostra = getControleAmostra(seqAmostra);
+                    BigDecimal codprod = controleAmostra.asBigDecimalOrZero("CODPROD");
+
+                    if (seqAmostra != null && sequencia != null && codprod != null) {
+                        EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+                        jdbcWrapper = dwfFacade.getJdbcWrapper();
+                        nativeSql = new NativeSql(jdbcWrapper);
+                        nativeSql.loadSql(getClass(), "query.sql");
+                        nativeSql.setNamedParameter("CODPROD", codprod);
+                        nativeSql.setNamedParameter("SEQAMOSTRA", seqAmostra);
+                        nativeSql.setNamedParameter("SEQUENCIA", sequencia);
+                        ResultSet rs = nativeSql.executeQuery();
+                        while (rs.next()) {
+                            BigDecimal resultadoAnaliseQuimica = new BigDecimal(resultadoAnaliseQuimicaString);
+                            String garantiaString = rs.getString("PERCFORMULA");
+                            BigDecimal garantia = new BigDecimal(garantiaString);
+                            BigDecimal formula = rs.getBigDecimal("FORMULA");
+                            BigDecimal formulaDois = rs.getBigDecimal("FORMULADOIS");
+                            BigDecimal resultadoDentroTolerancia;
+                            if ((garantia.compareTo(BigDecimal.valueOf(40)) == 1) || garantia.compareTo(BigDecimal.valueOf(40)) == 0) {
+                                tolerancia = garantia.subtract(formula);
+                                resultadoDentroTolerancia = garantia.subtract(tolerancia);
+                            } else {
+                                tolerancia = garantia.multiply(formula).add(formulaDois);
+                                resultadoDentroTolerancia = garantia.subtract(tolerancia);
+                            }
+                            BigDecimal deficienciaGrave = garantia.subtract(tolerancia.multiply(BigDecimal.valueOf(1.5)));
+                            BigDecimal deficienciaGravissima = garantia.subtract(tolerancia.multiply(BigDecimal.valueOf(3)));
+                            if ((resultadoAnaliseQuimica.compareTo(resultadoDentroTolerancia) == 1) || (resultadoAnaliseQuimica.compareTo(resultadoDentroTolerancia) == 0)) {
+                                registroSelcionado.setCampo("RESULTADOFORMULA", tolerancia);
+                                registroSelcionado.setCampo("RESULTADOTOL", '4');
+                            } else if ((resultadoAnaliseQuimica.compareTo(resultadoDentroTolerancia) == -1) && resultadoAnaliseQuimica.compareTo(deficienciaGrave) == 1) {
+                                registroSelcionado.setCampo("RESULTADOFORMULA", tolerancia);
+                                registroSelcionado.setCampo("RESULTADOTOL", '1');
+                            } else if (((resultadoAnaliseQuimica.compareTo(deficienciaGrave) == -1) && resultadoAnaliseQuimica.compareTo(deficienciaGravissima) == 1) || resultadoAnaliseQuimica.compareTo(deficienciaGrave) == 0) {
+                                registroSelcionado.setCampo("RESULTADOFORMULA", tolerancia);
+                                registroSelcionado.setCampo("RESULTADOTOL", '2');
+                            } else {
+                                registroSelcionado.setCampo("RESULTADOFORMULA", tolerancia);
+                                registroSelcionado.setCampo("RESULTADOTOL", '3');
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            MGEModelException.throwMe(e);
+        } finally {
+            JapeSession.close(hnd);
+        }
+
+
+    }
+
+
+    private DynamicVO getControleAmostra(BigDecimal seqAmostra) throws Exception {
+        JapeWrapper DAO = JapeFactory.dao("AD_CTLAMOSTRA");
+        DynamicVO Vo = DAO.findOne("SEQAMOSTRA = ?", new Object[]{seqAmostra});
+        return Vo;
+    }
+}
