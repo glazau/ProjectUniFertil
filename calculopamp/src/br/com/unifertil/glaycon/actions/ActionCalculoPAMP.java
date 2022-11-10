9f518f461fefedbf2e2212fa3b60a275889ed6d9
glazau <glaycon.hbp@gmail.com>
2022-11-10 10:14:08 -0300

    alteração feita no nome do pacote para melhor organização do projeto

--- calculopamp/src/br/com/unifertil/glaycon/actions/ActionCalculoPAMP.java
+++ calculopamp/src/br/com/unifertil/glaycon/actions/ActionCalculoPAMP.java
@@ -20,7 +20,7 @@ public class ActionCalculoPAMP implements AcaoRotinaJava {
     @Override
     public void doAction(ContextoAcao contextoAcao) throws Exception {
         JapeSession.SessionHandle hnd = null;
-        JdbcWrapper jdbcWrapper = null;
+        JdbcWrapper jdbcWrapper;
         NativeSql nativeSql = null;
         try {
             hnd = JapeSession.open();
